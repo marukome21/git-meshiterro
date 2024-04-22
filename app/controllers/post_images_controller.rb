0299157d213b8bbcbb169ec0e6c9b60333e20d27
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-22 14:29:23 +0000

    [Finish] 4-19 SNSらしい機能を追加しよう(2) - いいね機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -20,7 +20,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.delete
